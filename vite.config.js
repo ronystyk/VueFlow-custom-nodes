ebaeb1f5c000cf43fc442fa815c164064f842891
Rony Styk Muñoz Diaz <rmunos@truora.com>
2025-08-12 15:47:13 -0500

    Fix base URL configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  // Equivalente a publicPath en Webpack
+  base: '/VueFlow-custom-nodes/',
   plugins: [vue()],
   server: {
     port: 3000,
